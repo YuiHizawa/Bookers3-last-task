0bed51578de0d153f465e01b9b01c4c10a7d493c
檜澤ゆい <hizawayui@gmail.com>
2020-05-30 13:03:47 +0900

    edit

--- config/initializers/application_controller_renderer.rb
+++ config/initializers/application_controller_renderer.rb
@@ -6,3 +6,4 @@
 #     https: false
 #   )
 # end
+Refile.secret_key = 'e8e7c83187da3f69d3766cb37020fc55f671502896aae7f2ede3863e125a39ac756da1b81d28d12e5249c9bb349544704a7dfaca3980d9ff6ddce5411423ac68'
--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,13 @@
 Rails.application.routes.draw do
-  get 'homes/top'
+
   get 'homes/about'
   devise_for :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root 'homes#top'
 
-  resources :users, only: [:index, :show]
+  resources :users, only: [:index, :show, :edit, :update] 
 
-  resources :books, only: [:new, :create, :index, :show]
+  resources :books, only: [:new, :create, :index, :show, :edit, :update, :destroy]
+
+  resources :homes, only: [:top, :about]
 end
